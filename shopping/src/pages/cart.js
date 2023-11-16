0e241dd5caaf784bfb3e3a186178bcebf0848839
21255-ec-129 <velchoorigayathri@gmail.com>
2023-11-16 14:36:52 +0530

    first commit

--- shopping/src/pages/cart.js
+++
@@ -1,9 +0,0 @@
-import Cartitems from "../components/cartitems/cartitems "
-
-function Cart(){
-    return(
-        <div className="cart">
-<Cartitems/>
-        </div>
-    )
-}export default Cart
